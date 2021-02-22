9e3489e15995ea87f52ad9ecde21583bc0036c2d
fachrudahri <fachru.dahri@gmail.com>
2021-02-23 02:13:32 +0700

    update landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap'
+      }
     ]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -17,8 +17,8 @@
         </div>
       </div>
     </section>
-    <section class="container mb-5 pt-6 pb-6">
-        <h3 class="title is-3 has-text-centered">Team</h3>
+    <section class="container mb-5 pt-6 pb-6 mt-6 mb-6">
+        <h3 class="title is-3 has-text-centered">Tim Tribun EO</h3>
       <div class="container is-fluid level">
         <div class="level-item has-text-centered">
           <div class="mt-5">
@@ -54,10 +54,204 @@
         </div>
       </div>
     </section>
-    <section class="pt-6 pb-6 has-background-link">
+    <section class="pt-6 pb-6 mt-6 mb-6">
       <div class="container">
         <div class="container is-fluid">
-          <h3 class="title is-3 has-text-centered has-text-light">New Posts</h3>
+          <h3 class="title is-3 has-text-centered">Siapa Kami</h3>
+          <div class="columns is-vcentered is-multiline is-centered">
+            <div class="column is-4">
+              <figure class="image">
+              <img src="@/assets/setiawan.png">
+            </figure>
+            </div>
+            <div class="column is-8">
+              <p class="has-text-justified mb-3">
+                Merupakan bagian dari  PT. Harian Tribun Medan dan Kompas Gramedia Group yang merupakan perusahaan yang bergerak di bidang media cetak, TV, Radio, toko buku, dan lain-lain dengan network baik lokal maupun nasional yang terbesar di indonesia.
+              </p>
+              <p class="has-text-justified mb-3">
+                Merupakan Divisi specialist bergerak dibidang Event Organizer. Dengan memanfaat Jaringan Kompas Gramedia, Tribun EO memastikan setiap EVENT dan BRAND EXPOSURE akan maksimal dengan dukungan Jaringan Media Cetak Terbesar Tribun Medan, Jaringan Digital Terbesar Tribun-medan.com serta Multi Media melalui Jaringan Radio Sonora, Smart FM serta Kompas TV serta Media luar ruang lainnya.
+              </p>
+              <p class="has-text-justified mb-3">
+                Tim yang telah teruji pengalamannya baik skala local maupun nasional, senantiasa dapat menghadirkan keuntungan dan kepuasan diantara Brand dan Konsumen sehingga memaksimalkan sales serta Promotion.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="container mb-5 pt-6 pb-6 mt-6 mb-6">
+        <h3 class="title is-3 has-text-centered">Kenapa Memilih Kami</h3>
+      <div class="container is-fluid level">
+        <div class="columns">
+          <div class="column">
+            <div class="level-item has-text-centered">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <p class="has-text-centered mt-3">Kami adalah patner yang dapat dipercaya untuk  solusi event anda</p>
+          </div>
+          <div class="column">
+            <div class="level-item has-text-centered">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <p class="has-text-centered mt-3">Kami dapat mengcover area event di kota-kota besar di Indonesia</p>
+          </div>
+          <div class="column">
+            <div class="level-item has-text-centered">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <p class="has-text-centered mt-3">Kami dapat mempublikasikan event anda baik cetak maupun online</p>
+          </div>
+          <div class="column">
+            <div class="level-item has-text-centered">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <p class="has-text-centered mt-3">Kami menyediakan tempat untuk outdoor advertising termasuk produksi, izin dan laporan</p>
+          </div>
+          <div class="column">
+            <div class="level-item has-text-centered">
+              <figure class="image">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <p class="has-text-centered mt-3">Portal media online kami terintegrasi secara nasional melalui TribunNews.com</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="pt-6 pb-6 mt-6 mb-6">
+      <div class="container">
+        <div class="container is-fluid">
+          <h3 class="title is-3 has-text-centered">Kami Melayani</h3>
+          <div class="columns is-vcentered is-multiline is-centered">
+            <div class="column is-6 content is-medium">
+              <ul>
+                <li>PROMOTION & LAUNCHING PRODUK</li>
+                <li>EXHIBITION</li>
+                <li>SEMINAR & TALKSHOW</li>
+                <li>GATHERING</li>
+                <li>AGENCY (Entertainment & Talent)</li>
+                <li>PRODUCTION (Event Equipment, Branding Production)</li>
+                <li>OUTDOOR ADVERTISING (Billboard, Baligho, Store Branding)</li>
+              </ul>
+            </div>
+            <div class="column is-6 content is-medium">
+              <ul>
+                <li>PROMOTION & LAUNCHING PRODUK</li>
+                <li>EXHIBITION</li>
+                <li>SEMINAR & TALKSHOW</li>
+                <li>GATHERING</li>
+                <li>AGENCY (Entertainment & Talent)</li>
+                <li>PRODUCTION (Event Equipment, Branding Production)</li>
+                <li>OUTDOOR ADVERTISING (Billboard, Baligho, Store Branding)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="container is-fluid">
+          <h3 class="title is-3 has-text-centered">Client Kami</h3>
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://alfamidiku.com/assets/images/logo.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://upload.wikimedia.org/wikipedia/commons/9/9e/ALFAMART_LOGO_BARU.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://2.bp.blogspot.com/-YTawnQ1PX84/VKGLHcLbDcI/AAAAAAAAA3s/GLiU3EbX5vI/w1200-h630-p-k-no-nu/logo%2Bindomart.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Lotte_Mart_2018.svg/1672px-Lotte_Mart_2018.svg.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://www.pertamina.com/landing/images/logo.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://lh3.googleusercontent.com/proxy/Nb1pNmSZi8Kf1jcQjed2WASygcWnEbcyRp6YJ5_nThrM0BtHIPJg8dqOueDCFwmVLDIOdobF-Pw3yC7EfUIZfpTKN2ZnglYupMJM0USatKO8">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img class="is-rounded" src="https://www.freepnglogos.com/uploads/logo-telkomsel-png/telkomsel-communication-logos-2.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://4.bp.blogspot.com/-eqLw7lRJl4E/XCx40wjsz-I/AAAAAAAARPE/hlgk7_BNoZQXfyVb3jx0__RWdRrtI_T9wCLcBGAs/s1600/Kapal%2BApi.png">
+              </figure>
+            </div>
+          </div>
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              <figure class="image">
+                <img src="https://mayora.com/media/1175/logo-torabika-01.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://kalcare.s3-ap-southeast-1.amazonaws.com/moch4/uploads/brands/obrand-hydro.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/92/New_Logo_JNE.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://pbs.twimg.com/profile_images/704274220545548288/6YcYc_7r_400x400.jpg">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/92/Sophie_Logo_2018.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://1.bp.blogspot.com/-4zz2bPy31qk/XoMiJVaxy_I/AAAAAAAAB2g/hfgv3BGxV0EMT4YSlzZIlPB35nplBe86ACLcBGAsYHQ/s1600/Logo%2BHonda%2BOne%2BHeart.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://static.wikia.nocookie.net/logopedia/images/d/d9/Plossa.png/revision/latest?cb=20200131111333">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <img src="https://www.freepnglogos.com/uploads/yamaha-png-logo/world-brand-yamaha-png-logo-14.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="pt-6 pb-6 mt-6 mb-6">
+      <div class="container">
+        <div class="container is-fluid">
+          <h3 class="title is-3 has-text-centered">Postingan</h3>
           <div class="columns is-multiline">
             <div v-for="(post, index) in entries" :key="index" class="column is-4">
               <div v-if="index < 3">
